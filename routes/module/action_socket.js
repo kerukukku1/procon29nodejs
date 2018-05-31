b273bb42b747112e806e45a13ae176afe51fcabb
Mahito Fujino <kerukukku1@gmail.com>
2018-05-31 22:35:38 +0900

    Canvasの初期設定のバグを解消。プレーヤーの初期位置を描画するように変更

--- routes/module/action_socket.js
+++ routes/module/action_socket.js
@@ -3,48 +3,60 @@ var canvas = document.getElementById("myCanvas");
 // console.log(dir);
 var ctx = canvas.getContext("2d");
 var square_size = 50
-var w = square_size * 16;
-var h = w;
+var w;
+var h;
 var pos;
 var state = [];
-
+initCanvas();
 //サーバにファイル要求を出してファイルを取得
-socket.on('connect', function() {
+function initCanvas() {
   socket.emit('readfile', dir);
-  socket.on('filedata', function(data){
-    console.log(data.text);
+  socket.on('filedata', function(data) {
+    var arr = data.text.split('\n');
+    console.log(arr);
+    var turn = parseInt(arr[0]);
+    w = parseInt(arr[1]);
+    h = parseInt(arr[2]);
+    canvas.width = w * square_size+1.5;
+    canvas.height = h * square_size+1;
+    ctx.strokeStyle = "#757575"
+    ctx.lineWidth = 0.5;
+    ctx.translate(0.5, 0.5);
+    ctx.textAlign = "center";
+    var colors = ["#FF4081", "#03A9F4", "#FFFFFF"];
+    var i;
+    for (i = 0; i < h; i++) {
+      var row = arr[3 + i];
+      var elems = row.split(' ');
+      for (var j = 0; j < w; j++) {
+        var score = elems[j];
+        paintCell(j*square_size, i*square_size, score, colors[2]);
+      }
+      state.push(elems);
+    }
+    //bias
+    i+=3;
+    var npos;
+    npos = arr[i].split(' ').map(e => parseInt(e));
+    paintCell(npos[0]*square_size, npos[1]*square_size, state[npos[1]][npos[0]], colors[0]);
+    npos = arr[i+1].split(' ').map(e => parseInt(e));
+    paintCell(npos[0]*square_size, npos[1]*square_size, state[npos[1]][npos[0]], colors[0]);
+    npos = arr[i+2].split(' ').map(e => parseInt(e));
+    paintCell(npos[0]*square_size, npos[1]*square_size, state[npos[1]][npos[0]], colors[1]);
+    npos = arr[i+3].split(' ').map(e => parseInt(e));
+    paintCell(npos[0]*square_size, npos[1]*square_size, state[npos[1]][npos[0]], colors[1]);
   });
-});
-canvas.width = w;
-canvas.height = h;
-ctx.strokeStyle = "#757575"
-ctx.lineWidth = 0.5;
-ctx.translate(0.5, 0.5);
-ctx.textAlign = "center";
-var colors = ["#FF4081", "#03A9F4"];
-for (var i = 0; i < 16; i++) {
-  var objs = [];
-  for (var j = 0; j < 16; j++) {
-    var rnd = Math.random() * 10;
-    var score = parseInt(Math.random() * 33 - 16, 10);
-    var obj = {
-      color: (rnd < 5 ? 0 : 1),
-      score : score
-    };
-    objs[j] = obj;
-    ctx.fillStyle = colors[obj.color]
-    var nowx = j * square_size;
-    var nowy = i * square_size;
-    ctx.font = "20px bold";
-    ctx.fillRect(nowx, nowy, square_size, square_size);
-    ctx.rect(nowx, nowy, square_size, square_size);
-    ctx.stroke();
-    ctx.fillStyle = "white";
-    ctx.fillText(score, nowx+(square_size/2), nowy+(square_size/2), 1000);
-  }
-  state.push(objs);
 }
 
+function paintCell(posx, posy, score, c){
+  ctx.fillStyle = c;
+  ctx.font = "20px bold";
+  ctx.fillRect(posx, posy, square_size, square_size);
+  ctx.rect(posx, posy, square_size, square_size);
+  ctx.stroke();
+  ctx.fillStyle = "#333333";
+  ctx.fillText(score, posx + (square_size / 2), posy + (square_size / 2), 1000);
+}
 
 canvas.addEventListener("mousedown", function(event) {
   pos = getPos(event);
@@ -56,29 +68,29 @@ socket.on('connect', function() {
   socket.on('square', function(data) {
     console.log("on draw : " + data);
     var coord = whoSquare(data);
-    var c = (state[coord.y][coord.x].color+1)%2;
+    var c = (state[coord.y][coord.x].color + 1) % 2;
     state[coord.y][coord.x].color = c;
     ctx.fillStyle = colors[c];
-    var nowx = coord.x*square_size;
-    var nowy = coord.y*square_size;
+    var nowx = coord.x * square_size;
+    var nowy = coord.y * square_size;
     var score = state[coord.y][coord.x].score;
     ctx.fillRect(nowx, nowy, square_size, square_size);
     ctx.rect(nowx, nowy, square_size, square_size);
     console.log(nowx, nowy);
     ctx.stroke();
     ctx.fillStyle = "white";
-    ctx.fillText(score, nowx+(square_size/2), nowy+(square_size/2), 1000);
+    ctx.fillText(score, nowx + (square_size / 2), nowy + (square_size / 2), 1000);
   });
 });
 
-function whoSquare(data){
+function whoSquare(data) {
   var x = parseInt(data.x, 10);
   var y = parseInt(data.y, 10);
   var chx = parseInt(x / square_size, 10);
   var chy = parseInt(y / square_size, 10);
   return {
-    x : chx,
-    y : chy,
+    x: chx,
+    y: chy,
   }
 }
 
