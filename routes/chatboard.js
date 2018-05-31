fc8f18e281793a185381e0218bbfebc6cb0d3da5
Mahito Fujino <kerukukku1@gmail.com>
2018-05-31 18:39:37 +0900

    mysqlに関するファイルを追加。

--- routes/chatboard.js
+++ routes/chatboard.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var moment = require('moment');
+var connection = require('../mysqlConnection');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('chatboard', { title: 'ChatBoard' });
+});
+
+router.post('/', function(req, res, next) {
+  var title = req.body.title;
+  var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  console.log(title);
+  console.log(createdAt);
 });
 
 module.exports = router;
