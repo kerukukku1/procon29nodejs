7b8b49657cfa462613d2531a6819c88fb697138b
Mahito Fujino <kerukukku1@gmail.com>
2018-06-20 10:59:58 +0900

    リロード時に警告が出ないようにredirectを用いてpost情報を逐次消すように実装．

--- routes/rooms.js
+++ routes/rooms.js
@@ -7,9 +7,9 @@ router.get('/:room_id', function(req, res, next) {
     res.redirect('/');
   } else {
     var roomId = req.params.room_id;
-    if(isNaN(roomId)){
+    if (isNaN(roomId)) {
       res.render('error', {
-        roomid_error : true
+        roomid_error: true
       });
     }
     res.render('rooms', {
@@ -18,9 +18,13 @@ router.get('/:room_id', function(req, res, next) {
   }
 });
 
-router.post('/:roomId', function(req, res, next) {
+router.post('/:room_id', function(req, res, next) {
   console.log("POST!!!");
   console.log(req.body);
+  console.log(req.session.userid);
+  console.log(req.session.username);
+  var roomId = req.params.room_id;
+  res.redirect('/rooms/'+String(roomId));
 });
 
 module.exports = router;
